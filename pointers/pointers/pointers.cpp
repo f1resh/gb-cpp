c571b9c45ac7c70262c969312f26d637f3448ada
ARCADIA\pavel.akulov <pavel.akulov@arcadia.spb.ru>
2022-01-03 09:21:43 +0300

    Homework 6

---
+++ pointers/pointers/pointers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "pointers.h"
+
+using namespace std;
+
+
+
+int main()
+{
+    srand(time(NULL));
+
+    task_1();
+
+    task_2();
+
+    task_3();
+
+    task_4();
+
+    task_5();
+}
+
